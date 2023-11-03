6cba216ed05cf800a2a5a572a5b0ea9b376056c3
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2023-11-03 15:32:56 +0200

    add status circle

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,13 @@
 
 $(document).ready(function() {
+    $.get('http://localhost:5001/api/v1/status/')
+    .done(function(data) {
+        if (data['status'] === 'OK')
+            $('#api_status').addClass('available');
+    })
+    .fail(function() {
+        $('#api_status').addClass('notavailable');
+    });
     const amenities = {}
     $('.amenity-checkbox').on('change', function () {
         if ($(this).is(':checked')) {
